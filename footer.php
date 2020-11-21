d530e271bc3e950699fb2a5f9c83b6e8e55ed274
Mohit Kumar PatnaIK <39707697+mohitpatnaik@users.noreply.github.com>
2020-11-21 13:16:47 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+<title>Untitle document</title>
+</head>
+<body>
+<table width ="1316" bgcolor="red" style=" color:white;bottom:0px;font-size:20px;">
+<tr>
+<td class="f" align="center" >Basic Banking System </td>
+</tr>
+</table>
+</html>
